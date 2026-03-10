765a3111a57b920f266d39a5e8dca76b1e555e65
Sameerxh <32047689+Sameerxh@users.noreply.github.com>
2026-03-10 08:46:45 +0000

    adjusted companies subheading

--- companies.php
+++ companies.php
@@ -32,7 +32,7 @@ $companies = $result->fetchAll(PDO::FETCH_ASSOC);
         <div class="two-column">
             <div class="left-column">
                 <h2>Companies</h2>
-                 <h4>Select a company to view details</h4>
+                 <h4>Select a company below to view</h4>
 
                 <ul>
                      <?php foreach ($companies as $company) { ?>
