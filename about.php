f522fd71e358530d4594305862f554b2d070e205
Sameerxh <32047689+Sameerxh@users.noreply.github.com>
2026-03-10 06:58:43 +0000

    Updated about page

---
+++ about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Stock Portfolio - About</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+<body>
+    <header>
+        <h1>Stock Portfolio Tracker</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="companies.php">Companies</a>
+            <a href="about.php">About</a>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Abour</h2>
+        <p>This is <strong> Assignment #1 </strong> for COMP3512 at Mount Royal University. </p>
+
+        <h3>What This Site Does</h3>
+        <p>This website allows users to view customer stock portfolios and company information. 
+            Users can browse a list of customers and view their stock holding, including the total value of their portfolio. 
+            Users can also browse companies and view detailed stock history and statistics. 
+        </p>
+
+        <h3>Technologies Used</h3>
+        <ul>
+            <li>PHP - Server-side programming</li>
+            <li>PDO - Databse connection</li>
+            <li>SQLite - Database</li>
+            <li>HTML - Page structure</li>
+            <li>CSS - Styling</li>
+        </ul>
+
+        <h3>Created By</h3>
+        <p>Sameerah Ajenifuja</p>
+    </main>
+</body>
+</html>
